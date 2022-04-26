cd6b3a3af6a5c402945bea22e8b9485db87ebf23
baonguyen1222 <85530882+baonguyen1222@users.noreply.github.com>
2022-04-26 17:35:49 -0400

    Add global variables

---
+++ EZ-Template-main/src/globalVariables.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+
+//GLOBAL VARIABLES--------------------------------------------------------------
+bool driveLock = false;
+bool conveyorOn = false;
+
+int liftUpSpeed = 100;
+int liftDownSpeed = 100;
+
+int conveyorSpeed = 600; // prolly need to change
+
+
+bool clampIsDown = false;
+bool CanstealerIsDown = false;
